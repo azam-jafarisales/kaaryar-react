e35d43423de2173b3457fd4be2dc46f0d9f54b19
azam jafari <azamjs1990@gmail.com>
2023-12-14 11:16:47 +0330

    add font

--- azamjfari-tamrin8-1402.8.27/src/index.js
+++ azamjfari-tamrin8-1402.8.27/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/index.js';
 import reportWebVitals from './reportWebVitals';
+import "./Fonts/fonts.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
